f38649025d8bd51b7e829e8733e8f2b0284f0386
Tvrtko <tvrtkobabic2@gmail.com>
2024-12-25 22:55:26 +0100

    add deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  site: 'https://tvrtk0.github.io',
+  base: 'gsbj',
+  integrations: [],
+})
